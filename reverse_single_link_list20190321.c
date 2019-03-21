12cbaea310b8eb99ad0c84078d82a975e57f0429
sss-0916 <sss-0916@qq.com>
2019-03-21 08:59:13 +0800

    2019_03_21

---
+++ reverse_single_link_list20190321.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef int ElementType;
+
+typedef struct Node
+{
+    ElementType value;
+    struct Node* next;
+} Node;
+
+Node* sLLReverse(Node** first)
+{
+    assert(first != NULL);
+    Node* cur = *first;
+    Node* result = NULL;
+    while(cur != NULL)
+    {
+        Node* temp = cur->next;
+        cur->next = result;
+        result = cur;
+        cur = temp;
+    }
+    return result;
+}
